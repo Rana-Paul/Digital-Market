3bd2c1382c1f366c3f9e75aa78f2be272ffa018e
Rana-Mapscrew <ranapaul741223@gmail.com>
2023-12-01 00:15:33 +0530

    reset

---
+++ src/collections/Users.ts
@@ -0,0 +1,40 @@
+import { CollectionConfig } from "payload/types";
+
+const Users: CollectionConfig = {
+    slug: "users",
+    auth: {
+        verify: {
+            generateEmailHTML: ({token}) => {
+                return `<p>Hello Rana...</p>`
+                
+            }
+        }
+    },
+    access: {
+      read: () => true,
+      create: () => true,  
+    },
+    fields: [
+        {
+            name: 'role',
+            required: true,
+            defaultValue: 'user',
+            // admin: {
+            //   condition: () => false
+            // },
+            type: 'select',
+            options: [
+                {
+                    label: 'Admin',
+                    value: 'admin',
+                },
+                {
+                    label: 'User',
+                    value: 'user',
+                }
+            ]
+        }
+    ]
+}
+
+export default Users
--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -3,6 +3,6 @@ import { publicProcedure, router } from "./trpc";
 
 export const authRouter = router({
     createPayloadUser: publicProcedure.input(AuthCredentialsValidator).mutation(() => {
-        //signup
+        //signup next
     })
 });
