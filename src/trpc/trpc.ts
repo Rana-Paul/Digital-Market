de8ae7b1732663a87b13290a905e7820927cd8ab
Rana-Mapscrew <ranapaul741223@gmail.com>
2023-12-12 17:55:31 +0530

    payment-router

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,7 +1,26 @@
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
 import {ExpressContext} from '@/server';
+import { PayloadRequest } from "payload/types";
+import { User } from "@/payload-types";
 
 const t = initTRPC.context<ExpressContext>().create();
+const middleWare = t.middleware
+const isAuth = middleWare(async({ctx, next}) => {
+    const req = ctx.req as PayloadRequest
+    
+    const {user} = req as {user: User | null}
+
+    if(!user) {
+        throw new TRPCError({code: 'UNAUTHORIZED'})
+    }
+    return next({
+        ctx: {
+            user
+        }
+    })
+})
+
 export const router = t.router;
 
 export const publicProcedure = t.procedure;
+export const privateProcedure = t.procedure.use(isAuth);
