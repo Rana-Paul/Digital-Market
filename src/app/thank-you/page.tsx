4668adafae8124ec5d561ddc967198abac074d72
Rana-Mapscrew <ranapaul741223@gmail.com>
2023-12-16 18:23:42 +0530

    stripe webhook

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -5,8 +5,9 @@ import { getPayloadClient } from '@/get-payload'
 import { notFound, redirect } from 'next/navigation'
 import { Product, ProductFile, User } from '@/payload-types'
 import { PRODUCT_CATEGORIES } from '@/config'
-import { formatPrice } from '@/lib/utils'
+import { formatPrice } from '../../lib/utils'
 import Link from 'next/link'
+import PaymentStatus from '../../components/PaymentStatus'
 
 interface PageProps {
   searchParams: {
@@ -94,7 +95,111 @@ const ThankYouPage = async ({
               </p>
             )}
 
-            
+            <div className='mt-16 text-sm font-medium'>
+              <div className='text-muted-foreground'>
+                Order nr.
+              </div>
+              <div className='mt-2 text-gray-900'>
+                {order.id}
+              </div>
+
+              <ul className='mt-6 divide-y divide-gray-200 border-t border-gray-200 text-sm font-medium text-muted-foreground'>
+                {(order.products as Product[]).map(
+                  (product) => {
+                    const label = PRODUCT_CATEGORIES.find(
+                      ({ value }) =>
+                        value === product.category
+                    )?.label
+
+                    const downloadUrl = (
+                      product.product_files as ProductFile
+                    ).url as string
+
+                    const { image } = product.images[0]
+
+                    return (
+                      <li
+                        key={product.id}
+                        className='flex space-x-6 py-6'>
+                        <div className='relative h-24 w-24'>
+                          {typeof image !== 'string' &&
+                          image.url ? (
+                            <Image
+                              fill
+                              src={image.url}
+                              alt={`${product.name} image`}
+                              className='flex-none rounded-md bg-gray-100 object-cover object-center'
+                            />
+                          ) : null}
+                        </div>
+
+                        <div className='flex-auto flex flex-col justify-between'>
+                          <div className='space-y-1'>
+                            <h3 className='text-gray-900'>
+                              {product.name}
+                            </h3>
+
+                            <p className='my-1'>
+                              Category: {label}
+                            </p>
+                          </div>
+
+                          {order._isPaid ? (
+                            <a
+                              href={downloadUrl}
+                              download={product.name}
+                              className='text-blue-600 hover:underline underline-offset-2'>
+                              Download asset
+                            </a>
+                          ) : null}
+                        </div>
+
+                        <p className='flex-none font-medium text-gray-900'>
+                          {formatPrice(product.price)}
+                        </p>
+                      </li>
+                    )
+                  }
+                )}
+              </ul>
+
+              <div className='space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-muted-foreground'>
+                <div className='flex justify-between'>
+                  <p>Subtotal</p>
+                  <p className='text-gray-900'>
+                    {formatPrice(orderTotal)}
+                  </p>
+                </div>
+
+                <div className='flex justify-between'>
+                  <p>Transaction Fee</p>
+                  <p className='text-gray-900'>
+                    {formatPrice(1)}
+                  </p>
+                </div>
+
+                <div className='flex items-center justify-between border-t border-gray-200 pt-6 text-gray-900'>
+                  <p className='text-base'>Total</p>
+                  <p className='text-base'>
+                    {formatPrice(orderTotal + 1)}
+                  </p>
+                </div>
+              </div>
+
+              <PaymentStatus
+                isPaid={order._isPaid}
+                orderEmail={(order.user as User).email}
+                orderId={order.id}
+              />
+
+              <div className='mt-16 border-t border-gray-200 py-6 text-right'>
+                <Link
+                  href='/products'
+                  className='text-sm font-medium text-blue-600 hover:text-blue-500'>
+                  Continue shopping &rarr;
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/server.ts
+++ src/server.ts
@@ -1,49 +1,67 @@
-import express from 'express'
-import { getPayloadClient } from './get-payload'
-import { nextApp, nextHandler } from './next-utils'
+import express from "express";
+import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next-utils";
+import bodyParser from "body-parser";
 
-const app = express()
-const PORT = Number(process.env.PORT) || 3000
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
 
-import * as trpcExpress from '@trpc/server/adapters/express'
-import { appRouter } from './trpc'
-import {inferAsyncReturnType} from '@trpc/server'
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { appRouter } from "./trpc";
+import { inferAsyncReturnType } from "@trpc/server";
+import { IncomingMessage } from "http";
+import { stripeWebhookHandler } from "./webhooks";
 
-const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({
-    req,
-    res,
-})
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+});
 
-export type ExpressContext = inferAsyncReturnType<typeof createContext>
-    
+export type ExpressContext = inferAsyncReturnType<typeof createContext>;
+
+export type WebhookRequest = IncomingMessage & { rawBody: Buffer };
 
 const start = async () => {
-    const payload = await getPayloadClient({
-        initOptions: {
-            express: app,
-            onInit: async (cms) => {
-                cms.logger.info(`Admin URL ${cms.getAdminURL()}`)              
-            },
-        },
+  const webhookMiddlewaer = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer;
+    },
+  });
 
+  app.post("/api/webhook/stripe", webhookMiddlewaer, stripeWebhookHandler);
+
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+      },
+    },
+  });
+
+  app.use(
+    "/api/trpc",
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext,
     })
+  );
 
-    app.use('/api/trpc', trpcExpress.createExpressMiddleware({
-        router: appRouter,
-        createContext,
-    }))
+  //forword requests to nextjs
+  app.use((req, res) => nextHandler(req, res));
 
-    //forword requests to nextjs
-    app.use((req, res) => nextHandler(req, res));
+  nextApp.prepare().then(() => {
+    payload.logger.info(`Nextjs Started...`);
 
-    nextApp.prepare().then(() => {
-        payload.logger.info(`Nextjs Started...`);
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        `Nextjs app URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      );
+    });
+  });
+};
 
-        app.listen(PORT, async () => {
-            payload.logger.info(`Nextjs app URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`);
-        })
-    })
-        
-}
-
-start()
+start();
