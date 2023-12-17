49af65626c704100eaaa0a9c0da9e00f21285ff5
Rana-Mapscrew <ranapaul741223@gmail.com>
2023-12-17 20:18:24 +0530

    email fix

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -99,7 +99,7 @@ export const stripeWebhookHandler = async (
     // send receipt
     try {
       const data = await resend.emails.send({
-        from: 'DigitalMarket <fullstack@ranathedeveloper.site',
+        from: 'Digital Market <fullstack@ranathedeveloper.site>',
         to: [user.email],
         subject:
           'Thanks for your order! This is your receipt.',
