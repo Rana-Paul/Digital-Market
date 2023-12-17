8b2fb9921dd4c5b0e44c3123af68746174db73e9
Rana-Mapscrew <ranapaul741223@gmail.com>
2023-12-17 19:48:57 +0530

    image fix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        hostname: "localhost",
-        protocol: "http",
-        port: "3000",
-        pathname: "**",
-      },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+			{
+				protocol: "https",
+				hostname: "digital-market-production-00e2.up.railway.app",
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
